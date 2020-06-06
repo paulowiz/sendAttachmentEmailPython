9f54942710e9e168fad67c41f45b4bc202b15c3d
Paulo Mota <paaulowiz@gmail.com>
2020-06-06 14:57:47 -0300

    Now we can read all files inside and outside the folders and build a excel with log of files

--- __init__.py
+++ __init__.py
@@ -8,35 +8,63 @@ from datetime import datetime
 from controller import email as em
 from controller import functions as fc
 from dotenv import load_dotenv
+import pandas as pd
+
+fcp = fc.funcoes() 
+df =  pd.DataFrame(columns=['A'])
+
+def listar_pasta(pasta):
+    i = 0
+    tamanho_arquivo =0
+    subpastas = list()
+    if os.path.isdir(pasta):
+        items = os.listdir(pasta)
+        for item in items:
+            novo_item = os.path.join(pasta,item)
+            if os.path.isdir(novo_item):
+                subpastas.append(novo_item)
+                continue
+            caminho_completo = pasta+'\\'+item
+            nome_do_arquivo = item
+            data_e_hora_atuais = datetime.now()
+            data_arquivo = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+            diretorio = pasta
+            tamanho_arquivo = os.path.getsize(caminho_completo)
+            print(pasta+'\\'+item)
+            enviaArquivos(caminho_completo)
+            #os.remove(pasta+str(i))
+            log = caminho_completo+';'+data_arquivo+';'+diretorio+';'+str(nome_do_arquivo)+';'+str(tamanho_arquivo)
+            fcc = fc.funcoes() 
+            fcc.insereDf(df,log)
+                    
+        
+        for subpasta in subpastas:
+            listar_pasta(subpasta)
+                   
+    return df
+
+
 
 def executaRotina():
     load_dotenv()
-    pasta = os.getenv("PASTA")
-    if not len(os.listdir(pasta)) == 0:
-        for i in os.listdir(pasta):
-            if not i.endswith(".crdownload"):
-                print(pasta+str(i))
-                #try:
-                data_e_hora_atuais = datetime.now()
-                print(data_e_hora_atuais.strftime('%d/%m/%Y %H:%M'))
-                enviaArquivos(pasta+str(i))
-                os.remove(pasta+str(i))
-                #except:
-                #    print('Erro ao processar o arquivo!')
-                #    os.remove(pasta+str(i))
-        print('Rotina executada com sucesso!')
+    path = os.getenv("PASTA")
+    pastas = os.listdir(path)
+    for pasta in pastas:
+        if os.path.isdir(pasta):
+            listar_pasta(path+pasta)
+        else:
+            file = path+pasta
+            if not file.endswith(".exe") and not file.endswith(".zip"):
+                enviaArquivos(file)  
+    fcp.geraExcel(df,'logFRE','Logs')
 
+        
+        
 def  enviaArquivos(arquivo):
     with open('listaEmails.txt', 'r') as f:
         myfile = f.read().splitlines()
     f.close()
     print(myfile)
-            
-        #func = fc.funcoes()
-        #try:
-         #   file = func.convertToBinaryData(arquivo)
-        #except:
-         #   return print("Erro ao ler o arquivo.")
     for line in myfile:
         mail = em.email()
         mail.enviaEmail(line,arquivo)
--- controller/functions.py
+++ controller/functions.py
@@ -1,5 +1,7 @@
 from PIL import Image
-import io
+import io,os
+import pandas as pd
+import numpy as np
 
 class funcoes:
     def __init__(self): 
@@ -11,6 +13,24 @@ class funcoes:
         image.save(buf, format='PDF',save_all=True)
         byte_im = buf.getvalue()
         return byte_im
+    
+    def insereDf(self,df,row):  
+        insert_loc = df.index.max()
+        columns = df.shape
+        i = 0
+        for reg in row:
+            i = i +1
+        for r in range(columns[1]-i):
+            row.append('')    
+        if np.isnan(insert_loc):
+           df.loc[0] = row
+        else:
+           df.loc[insert_loc + 1] = row
+           
+    def geraExcel(self,df,nomeXls,pasta):
+        if not os.path.isdir(pasta):
+           os.makedirs(pasta)
+        df.to_excel(r'%s/%s.xls'%(pasta,nomeXls), index = False)
         
 pass
         
